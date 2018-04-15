50228fd0739671517a569ed9dafcd0839d6c0ece
Felipe Cortez <felipecortezfi@gmail.com>
2018-04-15 15:57:40 -0300

    Added django-debug-toolbar, removed unused urls.py

--- src/marks/settings.py
+++ src/marks/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+    'debug_toolbar',
     #'rest_framework',
 ]
 
@@ -62,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'marks.urls'
