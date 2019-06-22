1fd8e20d2b67264ca919dc5df8e43108b4bebbdf
Felipe Cortez <felipecortezfi@gmail.com>
2019-06-22 13:23:17 -0300

    Remove sync script

--- sync.sh
+++
@@ -1,2 +0,0 @@
-rsync -azP app/ felipecortez@felipecortez.net:/srv/www/marks;
-ssh -t felipecortez.net "source /srv/www/marks/marksenv/bin/activate && python3 /srv/www/marks/manage.py collectstatic && sudo supervisorctl restart marks"
